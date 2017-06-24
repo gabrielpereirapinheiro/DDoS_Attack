9003a0ad8866f0482f9f06342092def6919ffbed
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-06-23 22:32:14 -0300

    fim da sexta

--- controle_central.py
+++ controle_central.py
@@ -1,22 +1,25 @@
 import sys
 from socket import socket, AF_INET, SOCK_DGRAM
-
 from threading import Thread
 
-
+#Funcao que manda o comando
 def mandando():
-	SERVER_IP   = '192.168.43.208'
-	PORT_NUMBER = 5009
+
+	#Define o IP que vai enviar o ataque
+	SERVER_IP   = '192.168.43.165'
+
+	PORT_NUMBER = 13005
 	SIZE = 1024
-	print ("Test client sending packets to IP {0}, via port {1}\n".format(SERVER_IP, PORT_NUMBER))
+	print ("Enviando para o IP {0} na porta {1}\n".format(SERVER_IP, PORT_NUMBER))
 	mySocket = socket( AF_INET, SOCK_DGRAM )
 
-	print 'ok'
-	while True:
-		mySocket.sendto('inicie_ataque',(SERVER_IP,PORT_NUMBER))
+	mySocket.sendto('pare_ataque',(SERVER_IP,PORT_NUMBER))
 
 
+
+#Thread que ira inicar o ataque
 mandando1 =Thread(target=mandando)
 
+#Comeca o ataque
 mandando1.start()
 
