e4a51598afc97eb8e145c0c9e04bfab9a966d960
iurebrandao <iurebrandao@gmail.com>
2017-06-23 21:46:04 -0300

    Arrumando o servidor mestre

--- SynFlood/servidor_mestre.py
+++ SynFlood/servidor_mestre.py
@@ -73,7 +73,7 @@ class Interface:
         self.entry.delete(0, END)
 
         self.IP = None
-        serverSocket.sendto("B", (clientAddress, serverPort))
+        serverSocket.sendto("B/ ", (clientAddress, serverPort))
 
         self.message = "Type IP to be attacked:"
         self.label_text.set(self.message)
